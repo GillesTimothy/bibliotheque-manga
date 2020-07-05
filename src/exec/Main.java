b6ba6bbd51a4e17f5842c29cffbd3d482dd49192
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-07-05 02:12:34 +0200

    debut jfx splashscreen + interface connexion

--- src/exec/Main.java
+++ src/exec/Main.java
@@ -1,10 +1,40 @@
 package exec;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/fx/splashscreen.fxml"));
+		primaryStage.setTitle("Bibliothèque manga <3");
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		primaryStage.centerOnScreen();
+		
+		Stage otherStage = new Stage();
+		Parent root1 = FXMLLoader.load(getClass().getResource("/fx/connectionace.fxml"));
+		otherStage.setTitle("Bibliothèque manga <3");
+		Scene scene1  = new Scene(root1);
+		otherStage.setScene(scene1);		
+		otherStage.setResizable(false);
+		
+		//3 seconde pause avant cacher splashscreen
+		Thread.sleep(3000);
+		primaryStage.hide();
+		otherStage.show();
+		otherStage.centerOnScreen();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(" hello world !!! ");
+		launch(args);
 	}
 
 }
