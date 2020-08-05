a1f54b75f8eef01cb7103e5f601495b2740f9f8b
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-08-05 11:34:27 +0200

    connection db + debut interface graph

---
+++ src/Connecter.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class Connecter {
+	
+	Connection con;
+	public Connecter() {
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e) {
+			System.err.println(e); 
+		//pour afficher erreur
+		}	
+		try {
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/collection", "root", "");
+		} catch(SQLException e) {System.err.println(e);}
+	}
+	Connection obtenirconnexion() {
+		return con;
+	}
+
+}
