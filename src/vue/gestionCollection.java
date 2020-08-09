af9b21eefde59fae0733bc7d67fa6dc0430089ce
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-08-09 22:30:24 +0200

    debut mvc

--- src/vue/gestionCollection.java
+++ src/vue/gestionCollection.java
@@ -1,8 +1,5 @@
 package vue;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
@@ -14,6 +11,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.HeadlessException;
 import java.awt.Dimension;
 
 public class gestionCollection extends javax.swing.JFrame {
@@ -25,14 +23,26 @@ public class gestionCollection extends javax.swing.JFrame {
 	Statement stm;
 	ResultSet Rs;
 	DefaultTableModel model=new DefaultTableModel();
-	
+
+
 	String id;
     String titre;
     String auteur;
     String status;
     String nbrTome;
     
+    public gestionCollection(String id, String titre, String auteur, String status, String nbrTome)
+			throws HeadlessException {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.auteur = auteur;
+		this.status = status;
+		this.nbrTome = nbrTome;
+	}
+    
     public gestionCollection() {
+    	  	
         initComponents();
         
         model.addColumn("id");
@@ -514,5 +524,44 @@ public class gestionCollection extends javax.swing.JFrame {
     public javax.swing.JTextField txtpr;
     public javax.swing.JTextField txtre;
     public javax.swing.JComboBox txtre2;
+    
+    
+    public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getNbrTome() {
+		return nbrTome;
+	}
+
+	public void setNbrTome(String nbrTome) {
+		this.nbrTome = nbrTome;
+	}
+	
+    @Override
+	public String toString() {
+		return "gestionCollection [id=" + id + ", titre=" + titre + ", auteur=" + auteur + ", status=" + status
+				+ ", nbrTome=" + nbrTome + "]";
+	}
 
 }
