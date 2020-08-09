5c25ebfd91dd66169cfcd7e8e6bb46812aea1d7b
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-08-09 22:02:33 +0200

    suppression image + ajout package

--- src/controleur/splashscreen.java
+++ src/controleur/splashscreen.java
@@ -7,27 +7,14 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import vue.gestionCollection;
+
 public class splashscreen extends JFrame {
 
 	public JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					splashscreen frame = new splashscreen();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			
-		});
-	}
-
+	
+	public javax.swing.JLabel jLabel7;
+	public javax.swing.JLabel jLabel6;
 	/**
 	 * Create the frame.
 	 */
@@ -60,16 +47,28 @@ public class splashscreen extends JFrame {
                 jLabel7MouseClicked(evt);
             }
         });
+        
+        
+        
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					splashscreen frame = new splashscreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		});
 	}
 	
 	public void jLabel7MouseClicked(java.awt.event.MouseEvent evt) {
     	gestionCollection C=new gestionCollection();
     	C.setVisible(true);
-    }
+	}	
 	
-	
-	public javax.swing.JLabel jLabel7;
-	public javax.swing.JLabel jLabel6;
-
-
 }
---
+++ src/exec/Main.java
@@ -0,0 +1,17 @@
+package exec;
+
+import controleur.splashscreen;
+
+public class Main {
+	
+	public static void main(String args[]) {
+    	java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	splashscreen b=new splashscreen();
+            	b.setVisible(true);
+                //new gestionCollection().setVisible(true);
+            }
+        });
+    }
+	
+}
